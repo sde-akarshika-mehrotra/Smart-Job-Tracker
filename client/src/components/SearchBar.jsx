ac4b6f14e78ff66bb5cd32e9fa880cff5ff923d4
Akarshika Mehrotra <akarshikamehrotra44@gmail.com>
2026-07-20 19:40:31 +0530

    Correction

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ function SearchBar({search, setSearch}) {
     return (
         <input
         type = "text"
-        placeholder = "Search Comapany..."
+        placeholder = "Search Company..."
         value = {search}
         onChange={(e) => 
             setSearch(e.target.value)
